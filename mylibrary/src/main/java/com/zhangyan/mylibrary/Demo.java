0c0c39df737f35e1289dc16beb1cf1db7b281efc
zhangyan <zhangyan@tarenwang.com>
2017-11-10 14:31:16 +0800

    TestDemo

---
+++ mylibrary/src/main/java/com/zhangyan/mylibrary/Demo.java
@@ -0,0 +1,13 @@
+package com.zhangyan.mylibrary;
+
+/**
+ * Created by zhangYan on 2017/11/10.
+ */
+
+public class Demo {
+
+    public double add(double i, double j) {
+        return i + j;
+    }
+
+}
