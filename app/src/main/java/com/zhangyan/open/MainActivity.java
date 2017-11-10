50cbe22d21ada2c8dc0a8fa5febce950df26ec84
zhangyan <zhangyan@tarenwang.com>
2017-11-10 15:32:30 +0800

    UI

--- app/src/main/java/com/zhangyan/open/MainActivity.java
+++ app/src/main/java/com/zhangyan/open/MainActivity.java
@@ -1,13 +1,24 @@
 package com.zhangyan.open;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.zhangyan.mylibrary.Demo;
+import com.zhangyan.mylibrary.TestActivity;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Demo demo = new Demo();
+        demo.add(1.1, 1.1);
+        Intent intent = new Intent();
+        intent.setClass(this, TestActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
